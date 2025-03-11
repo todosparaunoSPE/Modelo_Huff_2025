785a0f98f093dd0a8dd57a49dc3d2b0218b54729
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-03-10 18:06:33 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 10 18:01:15 2025
+
+@author: jperezr
+"""
+
+import streamlit as st
+
+st.title("📍 Ubicaciones de CAP´s de las AFORE y propuesta de nuevos CAP's por cada entidad federativa de la República Mexicana, utilizando el modelo de Huff.")
